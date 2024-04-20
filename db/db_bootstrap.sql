46b4a98bee0fcbc36b28cbcad2b8a4ff6b036dea
Abhishiek Karthik <karthik.ab@northeastern.edu>
2024-04-19 21:25:59 -0700

    FitTrack FIrst Commit!

---
+++ db/db_bootstrap.sql
@@ -0,0 +1,31 @@
+-- This file is to bootstrap a database for the CS3200 project. 
+
+-- Create a new database.  You can change the name later.  You'll
+-- need this name in the FLASK API file(s),  the AppSmith 
+-- data source creation.
+create database IF NOT EXISTS fittrack;
+
+-- Via the Docker Compose file, a special user called webapp will 
+-- be created in MySQL. We are going to grant that user 
+-- all privilages to the new database we just created. 
+-- TODO: If you changed the name of the database above, you need 
+-- to change it here too.
+grant all privileges on fittrack.* to 'webapp'@'%';
+flush privileges;
+
+-- Move into the database we just created.
+-- TODO: If you changed the name of the database above, you need to
+-- change it here too. 
+use fittrack;
+
+-- Put your DDL 
+
+-- CREATE TABLE IF NOT EXISTS fav_colors (
+--   name VARCHAR(20),
+--   color VARCHAR(10)
+-- );
+
+
+
+
+-- SAMPLE DATA
